f5e4ada9918bcf48eb030649526be46a871b0800
Diveshj8 <dj8@njit.edu>
2021-12-08 01:16:36 -0500

    code for signup redirect corrected

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,13 +5,14 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save && @user.user_type == "staff"
+    if @user.save
     	session[:user_id] = @user.id
       case @user.user_type
       when "staff" then redirect_to "http://clinicmanagement-dev.us-east-2.elasticbeanstalk.com/", notice: "Successfully created account"
+      #when "patient" then redirect_to "http://clinicmanagement-dev.us-east-2.elasticbeanstalk.com/", notice: "Successfully created account"
       when "patient" then redirect_to "https://patient-kiosk.herokuapp.com/userdetails/", notice: "Successfully created account"
       when "nurse" then redirect_to "https://pmp-backend-angular.herokuapp.com/nurse?nurseId={user.id}", notice: "Successfully created account"
-      when default then redirect_to root_path, notice: "Successfully created account"
+      else redirect_to root_path, notice: "Successfully created account"
       end 
     else
       render :new
