dbb3bc7f620816a13cc6efa31259b994427d63fb
Diveshj8 <dj8@njit.edu>
2021-12-07 22:58:04 -0500

    paths added for nurse

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,8 +9,8 @@ class RegistrationsController < ApplicationController
     	session[:user_id] = @user.id
       case @user.user_type
       when "staff" then redirect_to "http://clinicmanagement-dev.us-east-2.elasticbeanstalk.com/", notice: "Successfully created account"
-      when "patient" then redirect_to "https://patient-kiosk.herokuapp.com/userdetails", notice: "Successfully created account"
-      when "doctor" then redirect_to "", notice: "Successfully created account"
+      when "patient" then redirect_to "https://patient-kiosk.herokuapp.com/userdetails/", notice: "Successfully created account"
+      when "nurse" then redirect_to "https://pmp-backend-angular.herokuapp.com/nurse?nurseId={user.id}", notice: "Successfully created account"
       when default then redirect_to root_path, notice: "Successfully created account"
       end 
     else
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       case user.user_type
       when "staff" then redirect_to "http://clinicmanagement-dev.us-east-2.elasticbeanstalk.com", notice: "Logged in successfully"
-      when "patient" then redirect_to "https://patient-kiosk.herokuapp.com/userconsent", notice: "Logged in successfully"
-      when "doctor" then redirect_to "", notice: "Logged in successfully"
+      when "patient" then redirect_to "https://patient-kiosk.herokuapp.com/userconsent/", notice: "Logged in successfully"
+      when "nurse" then redirect_to "https://pmp-backend-angular.herokuapp.com/nurse?nurseId={user.id}", notice: "Logged in successfully"
       end
     else
       flash[:alert] = "Invalid email or password"
