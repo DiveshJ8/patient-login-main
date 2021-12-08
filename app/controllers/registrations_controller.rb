8f1bf71c3765e432be3212f6ef1cdf6bc8e0c6ec
Diveshj8 <dj8@njit.edu>
2021-12-07 22:24:55 -0500

    patient signup route added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,7 @@ class RegistrationsController < ApplicationController
     	session[:user_id] = @user.id
       case @user.user_type
       when "staff" then redirect_to "http://clinicmanagement-dev.us-east-2.elasticbeanstalk.com/", notice: "Successfully created account"
-      when "patient" then redirect_to "", notice: "Successfully created account"
+      when "patient" then redirect_to "https://patient-kiosk.herokuapp.com/userdetails", notice: "Successfully created account"
       when "doctor" then redirect_to "", notice: "Successfully created account"
       when default then redirect_to root_path, notice: "Successfully created account"
       end 
